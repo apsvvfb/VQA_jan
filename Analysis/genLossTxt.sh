a410462ea457f647dc9677bd9125de37b31d0d43
Na Rong <c-nrong@gpu09.rlab.miniy.yahoo.co.jp>
2016-11-02 11:44:16 +0900

    I'm going to change code after this "push"

--- Analysis/genLossTxt.sh
+++ Analysis/genLossTxt.sh
@@ -2,6 +2,12 @@
 
 inputfile="train_with_engAttenmaps.txt"
 #inputfile="train_without_engAttenmaps.txt"
+
+outfile1=trainloss_with.txt
+outfile2=eval_with.res
+if [ -r $outfile1 ];then rm $outfile1;fi
+if [ -r $outfile2 ];then rm $outfile2;fi
+
 interval=12
 num=0
 del="accuracy"
@@ -11,18 +17,18 @@ do
 	if [ $[(num-1)%12] -eq 0 ]; then
 		iternum=`echo $line | cut -d':' -f1 | cut -d' ' -f2`
 		train_loss=`echo $line | cut -d' ' -f4 | cut -d',' -f1`
-		echo $train_loss
+		echo $train_loss >> $outfile1
 		continue
 	elif [ $[(num-2)%12] -eq 0 ]; then
 		val_loss=`echo $line | cut -d' ' -f3`
 		val_loss=${val_loss%${del}}
 		val_acc=`echo $line | cut -d' ' -f4`
-		#echo $iternum $val_loss $val_acc
+		echo $iternum $val_loss $val_acc >> $outfile2
 	elif [ $[(num-3)%12] -eq 0 ]; then
 		continue
 	else
 		train_loss=`echo $line | cut -d' ' -f4 | cut -d',' -f1`	
-		echo $train_loss
+		echo $train_loss >> $outfile1
 	fi
 done < $inputfile
 
